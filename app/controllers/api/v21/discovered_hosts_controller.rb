9278d365a7a7802bf8cdf2381bdbdddc72ab4544
Joseph Magen <jmagen@redhat.com>
2016-01-11 12:10:15 +0200

    add /apidoc/v21 docs for foreman_tasks, discovered_hosts, organizations, and lifecycle_environments

--- app/controllers/api/v21/discovered_hosts_controller.rb
+++ app/controllers/api/v21/discovered_hosts_controller.rb
@@ -4,14 +4,29 @@ module Api
 
       include Api::Version21
 
+      api :GET, "/discovered_hosts/", N_("List all discovered hosts")
+      param :search, String, :desc => N_("filter results")
+      param :order, String, :desc => N_("sort results")
+      param :page, String, :desc => N_("paginate results")
+      param :per_page, String, :desc => N_("number of entries per request")
+
       def index
         super
         @discovered_hosts = @discovered_hosts.where(:id => params[:id]) if params[:id].present?
         render :json => @discovered_hosts, :each_serializer => HostBaseSerializer
       end
 
+      api :GET, "/discovered_hosts/:id/", N_("Show a discovered host")
+      param :id, :identifier_dottable, :required => true
+
       def show
         render :json => @discovered_host, :serializer => HostBaseSerializer
+      end
+
+      api :PUT, "/discovered_hosts/:id/rename", N_("Rename a discovered host")
+      param :id, :identifier_dottable, :required => true
+      param :discovered_host, Hash, :action_aware => true do
+        param :name, String
       end
 
       # using rename rather than update since PUT update started the provision
--- app/controllers/api/v21/organizations_controller.rb
+++ app/controllers/api/v21/organizations_controller.rb
@@ -4,6 +4,9 @@ module Api
 
       include Api::Version21
 
+      api :GET, "/organizations/", N_("List of organizations")
+      param_group :search_and_pagination, ::Api::V2::BaseController
+
       def index
         # could not call super, since v2 calls render
         if @nested_obj
@@ -18,9 +21,20 @@ module Api
         render :json => @organizations, :each_serializer => OrganizationSerializer
       end
 
+      api :GET, "/organizations/:id/", N_("Show an organization")
+
       def show
         render :json => @organization, :serializer => OrganizationSerializer
       end
+
+      def_param_group :organization do
+        param :organization, Hash, :required => true, :action_aware => true do
+          param :name, String, :required => true, :desc => N_("Name of organization")
+        end
+      end
+
+      api :POST, "/organizations/", N_("Create a organization")
+      param_group :organization, :as => :create
 
       def create
         @organization = Organization.new(params[:organization])
